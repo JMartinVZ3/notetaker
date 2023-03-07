72891b86a29d804b0fa7d341e241f01bf9d00533
Martín <jmartinvz@outlook.com>
2023-03-06 20:44:54 -0600

    <feat>{added markdown, topics, and notes}

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { topicsRouter } from "~/server/api/routers/topics";
+import { notesRouter } from "~/server/api/routers/notes";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,8 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  topics: topicsRouter,
+  notes: notesRouter,
 });
 
 // export type definition of API
